e1bbe222763c66ea2b50f90f3db8adeeb9bca7b8
SystemGlitch <jeremy.la@outlook.fr>
2020-03-02 16:26:10 +0100

    Install WIP

---
+++ pages/install.vue
@@ -0,0 +1,254 @@
+<template>
+  <section class="section columns">
+    <div class="container column is-12">
+      <div class="card">
+        <div class="card-header">
+          <div class="columns card-header-title section-title">
+            <div class="column">
+              <h2 class="title is-3 has-text-gold">
+                Install
+                <b-icon icon="download" size="is-large" icon-size="mdi-48px" />
+              </h2>
+
+              <h4 class="subtitle has-text-grey-lighter">
+                Start your conquest now!
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div class="card-content">
+          <div class="columns">
+            <div class="column">
+              <div class="table-of-contents">
+                <h5 class="subtitle is-5 has-text-grey-lighter">
+                  Table of Contents
+                </h5>
+                <ul class="content">
+                  <li><a>1: Install The Original Game</a></li>
+                  <li>
+                    <ul>
+                      <li>
+                        <a>[OPTION 1] Install the original game using Steam</a>
+                      </li>
+                      <li>
+                        <a>
+                          [OPTION 2] Install the original game without using
+                          Steam
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a>2: Install Celeste Fan Project Patch</a></li>
+                  <li><a>3: Play AOEO Celeste Fan Project</a></li>
+                  <li>
+                    <ul>
+                      <li>
+                        <a>
+                          [OPTION 1] Play AOEO Celeste Fan Project Using Celeste
+                          Launcher
+                        </a>
+                      </li>
+                      <li>
+                        <a>
+                          [OPTION 2 (ADVANCED)] Play AOEO Celeste Fan Project
+                          Without Using Celeste Launcher
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a>4: Troubleshooting</a></li>
+                </ul>
+              </div>
+
+              <h4
+                id="install_the_original_game"
+                class="title is-4 has-text-gold"
+              >
+                1: Install The Original Game
+              </h4>
+
+              <h5
+                id="install_the_original_game_using_steam"
+                class="subtitle is-5 has-text-grey-lighter"
+              >
+                [OPTION 1] Install the original game using Steam
+              </h5>
+              <ul>
+                <li>
+                  <b>1</b>: You need to install
+                  <a
+                    href="http://store.steampowered.com/about/"
+                    rel="nofollow"
+                    target="_blank"
+                  >
+                    STEAM
+                  </a>
+                  and create an account if you haven't already.
+                </li>
+                <li>
+                  <b>2</b>: Follow this link to install "Age Of Empires Online"
+                  <a href="steam://install/105430" rel="nofollow">
+                    steam://install/105430
+                  </a>
+                  <br />
+                </li>
+              </ul>
+              <h5
+                id="install_the_original_game_without_using_steam"
+                class="subtitle is-5 has-text-grey-lighter"
+              >
+                [OPTION 2] Install the original game without using Steam
+              </h5>
+              <ul>
+                <li>
+                  <b>1</b>: You need to download the game using this link
+                  <a
+                    href="http://www.mediafire.com/file/afo6kcizm5qhl4e/AOEO_Installer.zip"
+                    rel="nofollow"
+                    target="_blank"
+                  >
+                    AOEO_Setup.exe
+                  </a>
+                </li>
+                <li>
+                  <b>2</b>: Once the download is complete you can install the
+                  game (You can install in the default installation directory,
+                  or you can install the game in a custom location).
+                </li>
+              </ul>
+
+              <h4
+                id="install_celeste_fan_project_patch"
+                class="title is-4 has-text-gold"
+              >
+                2: Install Celeste Fan Project Patch
+              </h4>
+              <ul>
+                <li>
+                  <b>1:</b> You need to download
+                  <a
+                    href="https://github.com/ProjectCeleste/Celeste_Launcher/releases/latest"
+                    rel="nofollow"
+                    target="_blank"
+                  >
+                    Celeste_Launcher_Gui.zip
+                  </a>
+                </li>
+                <li>
+                  <b>2:</b> Once the download is complete you need to extract
+                  all the content of the zip archive in a folder and copy it in
+                  the same folder has the game was installed, overwrite all
+                  existing file if system ask you (If you have used Steam to
+                  install the game the good folder will be "
+                  <span class="monospace">
+                    C:\Program Files\Steam\steamapps\common\Age Of Empires
+                    Online
+                  </span>
+                  " or "
+                  <span class="monospace">
+                    C:\Program Files (x86)\Steam\steamapps\common\Age Of Empires
+                    Online
+                  </span>
+                  ", and if you have used non-steam installer game will be
+                  located here (by default) "
+                  <span class="monospace">
+                    C:\Program Files\Age Of Empires Online
+                  </span>
+                  " or "
+                  <span class="monospace">
+                    C:\Program Files (x86)\Age Of Empires Online
+                  </span>
+                  ")
+                </li>
+                <li>
+                  <b>3:</b> Now search in game folder the executable
+                  "Celeste_Launcher_Gui" you just copied, right click on it then
+                  "Copy To &gt; Desktop (Shortcut)".
+                </li>
+              </ul>
+              <p>
+                <b>Note:</b> If you used Steam to install the game then click on
+                the Hammer in the launcher and then "Helpers &gt; Steam". After
+                executing the steam integration you can the launcher the game
+                with steam.
+              </p>
+
+              <h4
+                id="play_aoeo_celeste_fan_project"
+                class="subtitle is-4 has-text-gold"
+              >
+                3: Play AOEO Celeste Fan Project
+              </h4>
+
+              <h5
+                id="play_aoeo_celeste_fan_project_using_celeste_launcher"
+                class="subtitle is-5 has-text-grey-lighter"
+              >
+                [OPTION 1] Play AOEO Celeste Fan Project Using Celeste Launcher
+              </h5>
+              <ul>
+                <li>
+                  <b>1</b>: Launch "Celeste Launcher Gui" by using the shorcut
+                  on your desktop created during previous step
+                  <a rel="nofollow" target="_blank">
+                    (Install Celeste Fan Project Patch)
+                  </a>
+                  .
+                </li>
+                <li>
+                  <b>2</b>: Click on register if don't have already an account,
+                  if not go to step 4.
+                </li>
+                <li>
+                  <b>3</b>: Register your account (you need to use an valid
+                  email).
+                </li>
+                <li><b>4</b>: Login using your credetentials.</li>
+                <li>
+                  <b>5</b>: Set the game language in celeste launcher if
+                  incorrect.
+                </li>
+                <li><b>6</b>: Click "PLAY" and enjoy the game :).</li>
+              </ul>
+
+              <h5
+                id="play_aoeo_celeste_fan_project_without_using_celeste_launcher"
+                class="subtitle is-5 has-text-grey-lighter"
+              >
+                [OPTION 2 (ADVANCED)] Play AOEO Celeste Fan Project Without
+                Using Celeste Launcher
+              </h5>
+
+              <ul>
+                <li>
+                  <b>1</b>: You need to be already registered, if not use
+                  "Celeste Launcher Gui" to do it.
+                </li>
+                <li>
+                  <b>2</b>: Navigate to your game folder and create an shortcut
+                  for "Spartan.exe".
+                </li>
+                <li>
+                  <b>3</b>: Right click on the new shortcut, then press
+                  "Propriety" and edit "Target" field like in this example (you
+                  need to change credential with yours):
+                  <span class="monospace">
+                    "C:\Program Files (x86)\Age Of Empires Online\Spartan.exe"
+                    --email "mymail@mail.com" --password "mypassword1234"
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Install",
+  transition: "fade"
+}
+</script>
