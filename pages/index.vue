010cb78c5d6f3f8730a8cc392d07e34d31003b5d
SystemGlitch <jeremy.la@outlook.fr>
2020-03-08 12:50:18 +0100

    Landing page first draft

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,10 @@
     <parallax />
     <section
       id="after-parallax"
-      class="section hero is-fullheight has-text-centered is-relative"
+      class="section hero has-text-centered is-relative"
     >
       <div class="hero-body is-middle">
         <div class="container">
-          <div class="separator">
-            <img src="~/assets/img/separator.webp" />
-          </div>
           <h1 class="title has-text-gold is-1">
             Age of Empires Online
           </h1>
@@ -27,6 +24,111 @@
         </div>
       </div>
     </section>
+    <div class="separator">
+      <img src="~/assets/img/separator.webp" />
+    </div>
+    <section class="section">
+      <div class="container">
+        <div class="columns has-text-centered">
+          <div class="column is-4">
+            <p class="title is-4 has-text-gold">
+              <strong>Build your empire</strong>
+            </p>
+            <p class="content">
+              Choose from 7 civilizations and complete quests to level up your
+              civilization – unlocking new units, tech and challenges!
+            </p>
+          </div>
+          <div class="column is-4">
+            <p class="title is-4 has-text-gold">
+              <strong>Play with Friends</strong>
+            </p>
+            <p class="content">
+              Complete co-op quests with your friends or challenge their skills
+              in the PvP arena. Join the community and participate in
+              tournaments or gain high level gear in co-op quests!
+            </p>
+          </div>
+          <div class="column is-4">
+            <p class="title is-4 has-text-gold">
+              <strong>Gear up</strong>
+            </p>
+            <p class="content">
+              Customize your civilization and gear up your units by completing
+              quests solo or with friends. Complete challenging quests to earn
+              prestigious legendary gear!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div id="battle-separator" class="background-separator"></div>
+
+    <section class="section">
+      <div class="container has-text-centered">
+        <div class="columns">
+          <div class="column is-full">
+            <p class="title is-4 has-text-gold">
+              <strong>Completely Free!</strong>
+            </p>
+            <p class="content">
+              A fan-made community server that brings the excellent Age of
+              Empires Online back to life! Play every civilization for free!
+              free! There is no paid content – no need to buy anything!
+            </p>
+          </div>
+        </div>
+
+        <div class="columns has-text-centered">
+          <div class="column is-half">
+            <p class="title is-4 has-text-gold">
+              <strong>Classic experience</strong>
+            </p>
+            <p class="content">
+              Skip all of the PvE content and go right into gearless champion
+              mode PvP! The gameplay you know and love from Age of Empires is
+              all here. Our PvP balance team works tirelessly to provide the
+              game with a balanced, gearless, competitive mode.
+            </p>
+          </div>
+          <div class="column is-half">
+            <p class="title is-4 has-text-gold">
+              <strong>Quest, Loot, Craft!</strong>
+            </p>
+            <p class="content">
+              For those who loved the RTS MMO. It's all there. Gear up your
+              empire to take on more and more challenges. Play with friends in
+              horde (crete) mode. Take on the most difficult quests and gain
+              legendary gear. Build it from the ground up and show it off!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      id="play-hero"
+      class="hero is-success is-fullheight has-text-centered"
+    >
+      <div class="hero-body">
+        <div class="container is-flex">
+          <div class="card is-absolute-centered">
+            <div class="card-content">
+              <p class="title is-4">
+                Start your conquest
+              </p>
+              <nuxt-link
+                to="install"
+                class="button is-large title is-primary has-text-gold"
+              >
+                Play for free!
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -39,10 +141,39 @@ export default {
 
 <style lang="scss" scoped>
 #after-parallax {
-  background: linear-gradient(180deg, #1d1101 0%, $color--darker--opaque 50%);
+  background: linear-gradient(180deg, #1d1101 0%, $color--darker--opaque 100%);
+}
+
+.section {
+  background-color: $color--darker--opaque;
 }
 
 .separator {
-  margin: 6rem 0;
+  display: flex;
+  justify-content: center;
+  padding: 4rem 0;
+  background-color: $color--darker--opaque;
+}
+
+.columns:not(:last-child) {
+  margin-bottom: 2.25rem;
+}
+
+.background-separator {
+  height: 50vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+#battle-separator {
+  background-image: url("~assets/img/background_4.jpg");
+}
+
+#play-hero {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("~assets/img/background_14.jpg");
 }
 </style>
